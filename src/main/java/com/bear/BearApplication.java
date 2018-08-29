c4b317511c868aced206e436194dd57d85e54347
qianzhang <qianzhang@wacai.com>
2018-08-29 19:26:26 +0800

    add

--- src/main/java/com/bear/BearApplication.java
+++ src/main/java/com/bear/BearApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BearApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BearApplication.class, args);
 	}
